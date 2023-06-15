b0d145087bf330f1b7414c8ae870bc95427d6db2
Ali Ganji <aliganji142879@gmail.com>
2023-06-15 18:31:52 +0330

    add login and signup

--- config/settings.py
+++ config/settings.py
@@ -124,4 +124,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# authentication setting
 AUTH_USER_MODEL = "accounts.CustomUser"
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
