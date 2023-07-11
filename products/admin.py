b139cd4fdababd831567daa4ea733f3498957ccb
Ali Ganji <aliganji142879@gmail.com>
2023-07-11 19:08:54 +0330

    add view class base comment

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Comment
 
+
+class CommentsInline(admin.TabularInline):
+    model = Comment
+    fields = ["author", "body", "stars", "active"]
+    extra = 1
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("title", "price", "status")
+    inlines = [
+        CommentsInline,
+    ]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("product", "author", "body", "stars", "datetime_created", "datetime_modified", "active")
